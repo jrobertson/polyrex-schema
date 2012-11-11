9ea3562fec5cbe5a14657ddfc53164aee1b7e367
Your Name <james@r0bertson.co.uk>
2012-11-11 22:01:57 +0000

    feature: Rexle has now replaced REXML

--- lib/polyrex-schema.rb
+++ lib/polyrex-schema.rb
@@ -1,33 +1,39 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 # file: polyrex-schema.rb
 
-require 'rexml/document'
+require 'rexle'
 
 class PolyrexSchema
-  include REXML
 
   def initialize(s)
 
-    @doc = Document.new
+    @doc = Rexle.new
     node = @doc
 
     s.scan(/\w+\[[^\]]*\]|\w+/).each do |x|
+      
       b1, b2 = x.split('[')
+      
       if b2 then
-        node = append_node node, b1 do |summary|
+        
+        append_node node, b1 do |summary|
+          
           fields = b2.scan(/\w+(?=[,\]])/)
-          fields.each {|x| summary.add Element.new x}
-          summary.add Element.new('format_mask').add_text(fields.map {|x| "[!%s]" % x}. join(' '))
+          fields.each {|x| summary.add Rexle::Element.new x}
+          summary.add Rexle::Element.new('format_mask').add_text(fields.map {|x| "[!%s]" % x}. join(' '))
         end
+        
+        node = node.element '*/records'
+
       else
         node = append_node node, x
       end
     end
 
-    summary = XPath.first(@doc.root, 'summary')
-    summary.add_element Element.new('recordx_type').add_text('polyrex')
-    summary.add_element Element.new('schema').add_text(s)
+    summary = @doc.root.element 'summary'
+    summary.add_element Rexle::Element.new('recordx_type').add_text('polyrex')
+    summary.add_element Rexle::Element.new('schema').add_text(s)
 
   end
 
@@ -35,16 +41,23 @@ class PolyrexSchema
     @doc.to_s
   end
 
+  def to_doc
+    @doc
+  end
+
   private
 
   def append_node(node, name)
-    new_node = Element.new name
-    summary = Element.new 'summary'
+
+    new_node = Rexle::Element.new name
+    summary = Rexle::Element.new 'summary'
     yield(summary) if block_given?
+
     new_node.add summary
-    records = Element.new 'records'
+    records = Rexle::Element.new 'records'
     new_node.add records
+
     node.add new_node
-    records
+
   end
 end
--- polyrex-schema.gemspec
+++ polyrex-schema.gemspec
@@ -1,6 +1,8 @@
 Gem::Specification.new do |s|
   s.name = 'polyrex-schema'
-  s.version = '0.1.5'
+  s.version = '0.1.6'
   s.summary = 'The polyrex-schema gem creates Polyrex XML from an initial shorthand schematic string.'
+  s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
+  s.add_dependency('rexle')
 end
